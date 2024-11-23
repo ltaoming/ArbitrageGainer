c6ca353a9ab38459d573ea8de366dc218510ade7
Taoming Liu <taomingl@andrew.cmu.edu>
2024-11-22 18:44:25 -0800

    create start-trading endpoint and update cache mech

--- Program.fs
+++ Program.fs
@@ -3,45 +3,26 @@
 open Giraffe
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
-open Presentation.Handlers 
+open Presentation.Handlers
 open System
 open Presentation.CrossTradePairHandler
 open RealTimeMarketData
 open ArbitrageGainer.AnnualizedReturnCalc
+open Presentation.TradingHandler
 
 module Program =
     let webApp =
         choose [
             GET >=> route "/" >=> text "Hello World from Giraffe!"
             GET >=> route "/cross-traded-pairs" >=> getCrossTradedPairsHandler
+            POST >=> route "/start-trading" >=> TradingHandler.startTradingHandler
             Presentation.Handlers.webApp
             AnnualizedReturnApp().WebApp
         ]
 
     [<EntryPoint>]
     let main args =
-        let apiKey = "BKTRbIhK3OPX5Iptfh9pbpUlolQQMW2e" 
-        let uri = Uri("wss://socket.polygon.io/crypto")
-        // Define multiple subscription parameters
-        let subscriptionParametersList = [
-            "XT.BTC-USD"
-            "XT.ETH-USD"
-            "XT.LTC-USD"
-        ]
-
-        // Create a list of asynchronous start operations for each subscription
-        let connectionTasks =
-            subscriptionParametersList
-            |> List.map (fun params ->
-                PolygonWebSocket.start (uri, apiKey, params)
-            )
-
-        // Run all connections concurrently
-        Async.Parallel connectionTasks
-        |> Async.Ignore
-        |> Async.Start
-
-        printfn "Started multiple WebSocket connections."
+        printfn "WebSocket connections will start upon '/start-trading' endpoint call."
         
         Host.CreateDefaultBuilder()
             .ConfigureWebHostDefaults(fun webHost ->
