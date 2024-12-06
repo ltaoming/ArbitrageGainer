d54bd99c927bcca5816edb64a1c4ba98eefc661f
hungry5656 <hhhhhenrysun@gmail.com>
2024-12-06 00:17:21 -0800

    refactor: refactored order management that support the exchange api signatures

--- Services/Repository/OrderRepository.fs
+++ Services/Repository/OrderRepository.fs
@@ -83,12 +83,12 @@ let createOrder (order: Order):Result<string, string> =
     with
     | ex -> Error (ex.Message)
 
-let updateOrderStatus (orderId: string, status: string, filledQuantity: decimal):Result<string, string> =
+let updateOrderStatus (order: Order):Result<string, string> =
     try
-        let filter = Builders<OrderDto>.Filter.Eq((fun o -> o.OrderId), orderId)
+        let filter = Builders<OrderDto>.Filter.Eq((fun o -> o.OrderId), order.OrderId)
         let update = Builders<OrderDto>.Update
-                        .Set((fun o -> o.Status), status)
-                        .Set((fun o -> o.FilledQuantity), filledQuantity)
+                        .Set((fun o -> o.FilledQuantity), order.FilledQuantity)
+                        .Set((fun o -> o.Status), order.Status)
         let res = collection.UpdateOne(filter, update)
         Ok ("Success")
     with
