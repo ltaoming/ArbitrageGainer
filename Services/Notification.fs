9b09f6d3b3c02d38fc19bcceebe0a71352eacabb
LeChuan <lechuanm@andrew.cmu.edu>
2024-12-07 15:13:13 -0800

    change email recipient

--- Services/Notification.fs
+++ Services/Notification.fs
@@ -5,9 +5,9 @@ open EmailSender
 let notifyUserOfOrderStatusUpdate (orderId: string) (orderStatus: string) =
     let emailBody = sprintf "Order %s updated: %s" orderId orderStatus
     let emailSubject = "Order Status Changed"
-    EmailSender.sendEmail "pkotchav@andrew.cmu.edu" "lechuanm@andrew.cmu.edu" emailSubject emailBody |> ignore
+    EmailSender.sendEmail "pkotchav@andrew.cmu.edu" "pkotchav@andrew.cmu.edu" emailSubject emailBody |> ignore
 
 let notifyUserOfPLThresholdReached (threshold: decimal) =
     let emailBody = sprintf "P&L threshold reached: %M" threshold
     let emailSubject = "P&L Threshold has been reached"
-    EmailSender.sendEmail "pkotchav@andrew.cmu.edu" "lechuanm@andrew.cmu.edu" emailSubject emailBody |> ignore
+    EmailSender.sendEmail "pkotchav@andrew.cmu.edu" "pkotchav@andrew.cmu.edu" emailSubject emailBody |> ignore
